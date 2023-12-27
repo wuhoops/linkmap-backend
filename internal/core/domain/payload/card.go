c7d2bf0b98bcc623b49aca8175f1aae531bce0ef
wuhoops <95624019+wuhoops@users.noreply.github.com>
2023-12-28 03:02:12 +0700

    [FIX] card description omitempty

--- internal/core/domain/payload/card.go
+++ internal/core/domain/payload/card.go
@@ -3,7 +3,7 @@ package payload
 type Card struct {
 	CardId      string `json:"card_id"`
 	Topic       string `json:"topic"`
-	Description string `json:"description"`
+	Description string `json:"description,omitempty"`
 	Link        string `json:"link"`
 	OwnerId     string `json:"owner_id"`
 }
